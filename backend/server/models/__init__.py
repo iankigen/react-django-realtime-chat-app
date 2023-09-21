700f61e463f3e56f86674740dea5bc9bc228c6fe
Ian Kigen <ian.gabe.ian@gmail.com>
2023-09-21 11:44:58 +0300

    add server model

---
+++ backend/server/models/__init__.py
@@ -0,0 +1,4 @@
+from .base_model import BaseModel
+from .server import Server
+from .category import Category
+from .channel import Channel
---
+++ backend/server/models/server.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from .category import Category
+from .base_model import BaseModel
+
+User = get_user_model()
+
+
+class Server(BaseModel):
+    str_field = 'name'
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        User, related_name='servers', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='servers', on_delete=models.SET_NULL, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    members = models.ManyToManyField(User, related_name='membership')
+
