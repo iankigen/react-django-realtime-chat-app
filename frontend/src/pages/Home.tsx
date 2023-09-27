5179e1c314b7095e776ede8a879d0aef8cef15bc
Ian Kigen <ian.gabe.ian@gmail.com>
2023-09-27 07:34:54 +0300

    add AppBar to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import {Box, CssBaseline} from "@mui/material";
+import {PrimaryAppBar} from "./templates/PrimaryAppBar.tsx";
 
 const Home: React.FC = () => {
     return (
-        <>
-            <p>Home</p>
-        </>
+        <Box sx={{
+            display: "flex"
+        }}>
+            <CssBaseline/>
+            <PrimaryAppBar/>
+        </Box>
     )
 }
 
