58a7f427c3c8f34134ac277fe899dc64a0ac31fe
Ian Kigen <ian.gabe.ian@gmail.com>
2023-09-27 07:34:11 +0300

    use mui theme provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {RouterProvider} from "react-router-dom";
 import Router from "./router.tsx";
+import theme from "./theme/theme.tsx";
+import {ThemeProvider} from "@mui/material";
 
 const App: React.FC = () => {
     return (
-        <RouterProvider router={Router}/>
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={Router}/>
+        </ThemeProvider>
     )
 }
 export default App
