76bf199cfebf7b24c452629ead42835b2ed34c12
Ian Kigen <ian.gabe.ian@gmail.com>
2024-01-16 07:25:07 +0300

    update frontend router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,12 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
 import Home from "./pages/Home.tsx";
+import Explore from "./pages/Explore.tsx";
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path='/' element={<Home/>}>
-
+        <Route>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/explore/:categoryName' element={<Explore/>}/>
         </Route>
     )
 )
