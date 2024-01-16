ada9cf39ea361b95f406c8db07e8621dd8c5008d
Ian Kigen <ian.gabe.ian@gmail.com>
2024-01-16 07:25:18 +0300

    update theme

--- frontend/src/theme/theme.tsx
+++ frontend/src/theme/theme.tsx
@@ -34,6 +34,15 @@ declare module '@mui/material/styles' {
 const theme = createTheme({
     typography: {
         fontFamily: "'IBM Plex Sans',sans-serif",
+        body1: {
+            fontWeight: 500,
+            letterSpacing: "-0.5px",
+        },
+        body2: {
+            fontWeight: 500,
+            fontSize: "15px",
+            letterSpacing: "-0.5"
+        }
     },
     primaryAppBar: {
         height: 50,
